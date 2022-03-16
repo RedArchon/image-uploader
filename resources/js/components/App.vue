937ccb8e86f41dbb624ba1bd1d01adaad8fb4d29
Logan Collinsworth <logan@lynxtdc.com>
2022-03-16 16:46:12 -0400

    Updated ImageController show and store methods for handling new file uploads and populating the image gallery.
    Included CSRF token as part of the upload process in the headers rather than disabling the token verification middleware for added security.

---
+++ resources/js/components/App.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="mt-4">
+            <file-pond
+                name="image"
+                ref="pond"
+                label-idle="Click to choose image, or drag here...."
+                @init="filepondInitialized"
+                accepted-file-types="image/*"
+                @processfile="handleProcessedFile"
+            />
+        </div>
+        <div class="mt-8 mb-24">
+            <h2 class="text-2xl font-medium text-center">Image Gallery</h2>
+            <div class="grid grid-cols-3 gap-2 justify-evenly mt-4">
+                <div v-for="(image, index) in images" :key="index">
+                    <img :src="'/storage/images/'+ image" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import vueFilePond, {setOptions} from 'vue-filepond';
+import "filepond/dist/filepond.min.css";
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+setOptions({
+    server: {
+        process: {
+            url: './upload',
+            headers: {
+                'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf_token"]').content
+            }
+        }
+    }
+})
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+export default {
+    components: {
+        FilePond
+    },
+    data() {
+        return {
+            images: null
+        }
+    },
+    methods: {
+        filepondInitialized() {
+            console.log('Initialized Filepond Object:', this.$refs.pond);
+        },
+        handleProcessedFile(error, file) {
+            if (error) {
+                console.error(error)
+                return;
+            }
+
+            this.images.unshift(file.serverId);
+        }
+    },
+
+    mounted() {
+        axios.get('/images')
+            .then((response) => {
+                this.images = response.data;
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
